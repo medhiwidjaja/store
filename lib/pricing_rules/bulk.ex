0266b1603819d0bd601eec01d7dfd5c14553cfb4
Medhi Widjaja <medhi.widjaja@gmail.com>
2024-10-15 14:28:25 +0700

    Fraction price discount pricing rule on bulk purchase

--- lib/pricing_rules/bulk.ex
+++ lib/pricing_rules/bulk.ex
@@ -32,4 +32,47 @@ defmodule Store.PricingRule.Bulk do
       ]
     }
   end
+
+  @impl true
+  def apply(product, qty,
+        name: _,
+        type: "fraction",
+        min_qty: min_qty,
+        numerator: _,
+        denominator: _
+      )
+      when qty < min_qty do
+    full_price = D.mult(product.price, D.new(qty))
+
+    # Quantity doesn't meet threshold, so no discount added
+    %{
+      product: product,
+      qty: qty,
+      line_total: full_price
+    }
+  end
+
+  @impl true
+  def apply(product, qty,
+        name: name,
+        type: "fraction",
+        min_qty: _,
+        numerator: num,
+        denominator: dem
+      ) do
+    full_price = D.mult(product.price, D.new(qty))
+    fraction = D.div(D.new(num), D.new(dem))
+    discounted_price = product.price |> D.mult(fraction) |> D.mult(D.new(qty))
+
+    %{
+      product: product,
+      qty: qty,
+      line_total: full_price,
+      discounted_total: discounted_price,
+      discount: [
+        type: "#{name} discount",
+        amount: D.sub(full_price, discounted_price)
+      ]
+    }
+  end
 end
--- test/pricing_rules/bulk_test.exs
+++ test/pricing_rules/bulk_test.exs
@@ -14,6 +14,7 @@ defmodule Store.PricingRule.BulkTest do
     test "applies discount price on bulk order", %{product: product} do
       qty = 4
       discounted_price = D.new("1.5")
+      # Expected total = 1.5 * 4 = 6.0
       expected_total = D.new("6")
 
       %{discounted_total: total} =
@@ -43,7 +44,7 @@ defmodule Store.PricingRule.BulkTest do
       assert [type: "Promo1 discount", amount: expected_discount] == item[:discount]
     end
 
-    test "doesn't apply if qty doesn't meet minimum", %{product: product} do
+    test "doesn't apply rule if qty doesn't meet minimum", %{product: product} do
       qty = 2
       discounted_price = D.new("1.5")
 
@@ -53,6 +54,68 @@ defmodule Store.PricingRule.BulkTest do
           type: "fixed",
           min_qty: 3,
           price: discounted_price
+        )
+
+      assert item[:discount] == nil
+    end
+  end
+
+  describe "Fraction discount price Bulk Pricing Rule" do
+    setup do
+      coffee = %Product{code: "coffee", name: "Coffee", price: D.new("6.0")}
+      {:ok, %{product: coffee}}
+    end
+
+    test "applies discount price on bulk order", %{product: product} do
+      qty = 4
+      numerator = 2
+      denominator = 3
+      # Expected total = 6.0 * (2/3) * 4 = 16.0
+      expected_total = D.new("16")
+
+      %{discounted_total: total} =
+        Rule.apply(product, qty,
+          name: "Promo2",
+          type: "fraction",
+          min_qty: 3,
+          numerator: numerator,
+          denominator: denominator
+        )
+
+      assert D.eq?(total, expected_total)
+    end
+
+    test "adds discount info in line item", %{product: product} do
+      qty = 4
+      numerator = 2
+      denominator = 3
+      # Original total 24.0 minus Discounted total 16.0
+      expected_discount = D.new("8.0")
+
+      item =
+        Rule.apply(product, qty,
+          name: "Promo2",
+          type: "fraction",
+          min_qty: 3,
+          numerator: numerator,
+          denominator: denominator
+        )
+
+      assert D.eq?(expected_discount, item[:discount][:amount])
+    end
+
+    test "doesn't apply rule if qty doesn't meet minimum", %{product: product} do
+      qty = 1
+      numerator = 2
+      denominator = 3
+
+      item =
+        Rule.apply(product, qty,
+          name: "Promo2",
+          type: "fraction",
+          min_qty: 3,
+          numerator: numerator,
+          denominator: denominator
         )
 
       assert item[:discount] == nil
